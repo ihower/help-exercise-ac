6c1177f2334a19cb5b65a17ee8a1011dd7179f94
ChiuGypin <crsgypin@gmail.com>
2015-07-29 16:26:31 +0800

    api

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  # namespace "api" do
+  #   namespace "v1" do
+  #   end
+  # end
+
+  scope :path => '/api/v1/', :module => "api_v1", :defaults => { :format => :json }, :as => 'v1' do
+    resources :messages     
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
