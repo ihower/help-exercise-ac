c909c74a7eb2b592a919873c21af824c53104e93
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-11-05 16:31:32 +0800

    Q3 scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,15 @@ class Message < ActiveRecord::Base
 
   has_many :comments, :dependent => :destroy
 
+  scope :pending, -> { where( :status => "pending" ) }
+  scope :completed, -> { where( :status => "completed" ) }
+
+  def self.within_days(t=Time.now)
+      where(["created_at > ? ", t ])
+  end
+
+
+
   def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
   end
