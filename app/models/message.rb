b50b03d32f068d7653a69eec95b9915a3fc9dda8
Aron yu <h0935674938@hotmail.com>
2016-01-27 18:08:57 +0800

    提高效能

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,5 +7,9 @@ class Message < ActiveRecord::Base
   def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
   end
+  
+  scope :pending, -> {where(:status => "pending")}
+  scope :completed, -> {where( :status => "completed" )}
+  scope :within_days, ->(a){where(["created_at >= ?", Time.now - a.days ])}
 
 end
