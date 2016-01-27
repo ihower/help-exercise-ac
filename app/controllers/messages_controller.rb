cdb318bd5dc2ab96c78e02d5b92c4a5885adf7f7
SHOUTSE <frankchang1983@gmail.com>
2016-01-27 20:37:06 +0800

    use scope on message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,19 +4,20 @@ class MessagesController < ApplicationController
 
   def index
     # TODO: fix N+1 queries for user and comments
+    # use "includes"
     @messages = Message.includes(:user => :comments).order("id DESC").page( params[:page] )
 
     if params[:status] == "pending"
-      # TODO: @messages = @messages.pending
-      @messages = @messages.where( :status => "pending" )
+      @messages = @messages.pending
+      # @messages = @messages.where( :status => "pending" )
     elsif params[:status] == "completed"
-      # TODO: @messages = @messages.completed
-      @messages = @messages.where( :status => "completed" )
+      @messages = @messages.completed
+      # @messages = @messages.where( :status => "completed" )
     end
 
     if params[:days]
-      # TODO: @messages = @messages.within_days(params[:days].to_i)
-      @messages = @messages.where( ["created_at >= ?", Time.now - params[:days].to_i.days ] )
+      @messages = @messages.within_days(params[:days].to_i)
+      # @messages = @messages.where( ["created_at >= ?", Time.now - params[:days].to_i.days ] )
     end
   end
 
