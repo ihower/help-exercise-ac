93ba166657c513468ca478132753fd2653a4ea06
Dylan Lin <dylanmail0203@gmail.com>
2016-01-27 18:14:58 +0800

    6. 請實作一個 JSON API 網址是 /api/v1/messages，列出所有 Message 訊息，包括 id, status, category_name, title, content 和 created_at。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  scope :path => '/api/v1/', :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
+
+    resources :messages, :only => [:index]
+      
+  end
+
   devise_for :users
 
   resources :messages do
