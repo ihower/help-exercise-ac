a1bd8be4ec5d2f5bcd56e34d7888ee35c1f10399
Aron yu <h0935674938@hotmail.com>
2016-01-27 18:09:42 +0800

    回覆直接刪除

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728165437) do
+ActiveRecord::Schema.define(version: 20160127091720) do
+
+  create_table "apis", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
