3a679ed5b168f039882d0a9debb421c214f07134
plok74122 <plok74122@hotmail.com>
2016-04-29 17:07:42 +0800

    (新增多對多關聯) 使用者 User 可以訂閱(subscribe)和按贊(like)一則 Message：(7-1) 請新增 Subscription 和 Like 兩個 model，設定 user 和 message 的多對多關聯。(7-2) 接著修改 bundle exec rake dev:fake (檔案是 lib/tasks/dev.rake)，在產生假 Message 時，每則 Message 隨機挑兩個 user 做訂閱和按贊。然後再執行一次 bundle exec rake dev:fake 產生假資料 (7-3) 最後在 messages show 頁面上，顯示有誰訂閱和按贊。

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,13 @@ class Message < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :likes
+  has_many :like_users, :through => :likes, :source => :user
+
+  has_many :subscriptions
+  has_many :subscribed_users, :through => :subscriptions, :source => :user
+
+
   has_many :comments, :dependent => :destroy
   scope :pending, -> { where status: 'pending' }
   scope :completed, -> { where status: 'completed' }
