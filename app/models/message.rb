a1fbff61f84f80861d2d293e345f5b9f07c15565
ching-fu <gavaniyu@hotmail.com>
2016-04-29 16:00:57 +0800

    3. add scope in message controller & model

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,11 @@ class Message < ActiveRecord::Base
   belongs_to :user
 
   has_many :comments, :dependent => :destroy
+  scope :within_days,  lambda{ |t| where( ["created_at >= ?", Time.now-t.days ] )}    
+  scope :pending, -> {where( :status => "pending" )}
+  scope :completed, ->{where( :status => "completed" )}
+
+
 
   def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
