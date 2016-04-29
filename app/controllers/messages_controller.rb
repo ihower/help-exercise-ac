80ad0a02c3f5cbefd1598df650e16d3be057373a
J.CHO <jchobanana@gmail.com>
2016-04-29 16:52:24 +0800

    fix N+1 queries, API continue undone

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,15 +8,15 @@ class MessagesController < ApplicationController
 
     if params[:status] == "pending"
       # TODO: @messages = @messages.pending
-      @messages = @messages.where( :status => "pending" )
+      @messages = @messages.include(:user).where( :status => "pending" )
     elsif params[:status] == "completed"
       # TODO: @messages = @messages.completed
-      @messages = @messages.where( :status => "completed" )
+      @messages = @messages.include(:user).where( :status => "completed" )
     end
 
     if params[:days]
       # TODO: @messages = @messages.within_days(params[:days].to_i)
-      @messages = @messages.where( ["created_at >= ?", Time.now - params[:days].to_i.days ] )
+      @messages = @messages.include(:user).where( ["created_at >= ?", Time.now - params[:days].to_i.days ] )
     end
   end
 
