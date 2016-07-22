8a17043ca179feb4d9f33bcead3cd390924d6d8d
YengTsan <Yengttt@gmail.com>
2016-07-22 18:30:03 +0800

    build up relationship between user and message through like and subscription

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,13 @@ class Message < ActiveRecord::Base
   scope :status_completed, -> { where( :status => "completed" ) }
   scope :within_days, ->(days) { where( ["created_at >= ?", Time.now - days.to_i.days ] ) }
 
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
+
+  has_many :subscriptions
+  has_many :subscripted_users, through: :likes, source: :user
+
+
   belongs_to :user
 
   has_many :comments, :dependent => :destroy
