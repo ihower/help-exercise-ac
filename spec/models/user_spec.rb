39a5a86085b440f320b105a79090961fa91bf763
YengTsan <Yengttt@gmail.com>
2016-07-22 18:04:38 +0800

    finished unit test of #words_count

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,12 +24,13 @@ RSpec.describe User, type: :model do
 
   describe "#words_count" do
     before do
-      # TODO: 加 Message 和 Comment 測試資料
+      LOL = Message.create!( :user => @user, :content => "aaaaa bbbbb"  )
+      Comment.create!( :user => @user, :message => LOL, :content => "aaaaa bbbbb"  )
+      Comment.create!( :user => @user, :message => LOL, :content => "aaaaa bbbbb"  )
     end
 
     it "加總該使用者的所有 Mesasge 和 Comment 的總字數" do
-      # TODO: 測試 words_count 方法
-
+      expect( @user.words_count ).to eq(6)
     end
   end
 
