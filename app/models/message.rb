20ea3e40dd7cb28d292d0c32c3515ae5588204e6
YaoShangTseng <yakushou730@gmail.com>
2016-07-22 15:11:13 +0800

    finish #todo3 : refactor by using ActiveRecord Scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,4 +8,8 @@ class Message < ActiveRecord::Base
     self.comments.last.try(:content).try(:truncate, 20)
   end
 
+  scope :pending, -> { where(:status => "pending") }
+  scope :completed, -> { where( :status => "completed" ) }
+  scope :within_days, -> (t) { where("created_at >= ?", Time.now - t.days) }
+
 end
