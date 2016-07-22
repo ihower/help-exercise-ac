a5fb4cfa817006d9f1b7c40e7f9f215af8e161f5
YengTsan <Yengttt@gmail.com>
2016-07-22 17:36:55 +0800

    refactor messages query

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ActiveRecord::Base
 
+  scope :status_pending, -> { where( :status => "pending" ) }
+  scope :status_completed, -> { where( :status => "completed" ) }
+  scope :within_days, ->(days) { where( ["created_at >= ?", Time.now - days.to_i.days ] ) }
+
   belongs_to :user
 
   has_many :comments, :dependent => :destroy
