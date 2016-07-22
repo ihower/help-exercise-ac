6247fe4eb17ce5794bcefa11d1c5e79ad73b4584
Tim Wu <tim7012@msn.com>
2016-07-22 17:37:50 +0800

    remove html.erb for api index

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   scope :path => '/api/v1/', :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
-   resources :messages
+    resources :messages
   end
 
 
