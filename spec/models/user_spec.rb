f0d052acbb9264ae4c3965d0503fa28f8144283e
Xing <sky00000kimo@gmail.com>
2016-07-22 16:08:42 +0800

    #words_count rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,12 +24,14 @@ RSpec.describe User, type: :model do
 
   describe "#words_count" do
     before do
-      # TODO: 加 Message 和 Comment 測試資料
+      m2 = Message.create!( :user => @user ,:content => "test a 3432f258672__")
+      Comment.create!( :user => @user, :message => m2, :content =>"a great tea" )
+      Comment.create!( :user => @user, :message => m2, :content =>" 12345 + 88654 " )
     end
 
     it "加總該使用者的所有 Mesasge 和 Comment 的總字數" do
       # TODO: 測試 words_count 方法
-
+      expect( @user.words_count ).to eq(9)
     end
   end
 
