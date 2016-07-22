6b315db584b45019801fbe808b131f5cc8f70025
shanmon7799 <shanmon7799@hotmail.com>
2016-07-22 17:26:52 +0800

    fork projeck and add comment ajax and add scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,12 @@ class Message < ActiveRecord::Base
 
   has_many :comments, :dependent => :destroy
 
-  def last_comment_summary
+  scope :pending, -> { where status: "pending" }
+  scope :completed, -> {where status: "completed" }
+  scope :within_days, -> (time){ where "created_at >= ?", Time.now - time.days }
+
+
+	def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
   end
 
