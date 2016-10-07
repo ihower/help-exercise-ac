ed049b00b5d3f302854b4e8eefe79565eeeaab2f
A-hu <louis0911317399@gmail.com>
2016-10-07 22:16:13 +0800

    6. 請實作一個 JSON API

---
+++ app/controllers/api_v1/messages_controller.rb
@@ -0,0 +1,9 @@
+class ApiV1::MessagesController < ApplicationController
+
+	def index
+		messages = Message.all
+
+		render json: messages, only: [:id, :status, :category_name, :title, :content, :created_at]
+	end
+
+end
