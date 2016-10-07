3d3a4fa1c20d13da404bf4869ed5376e58a5ff73
ellenlee <leechinghui.tw@gmail.com>
2016-10-07 21:47:41 +0800

    index query with ActiveRecord Scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,10 @@ class Message < ActiveRecord::Base
 
   has_many :comments, :dependent => :destroy
 
+	scope :completed, -> { where( :status => "completed" ) }
+	scope :pending, -> { where( :status => "pending" ) }
+	scope :within_days, -> (date){ where( "created_at >= ?", Time.now - date.days ) }
+
   def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
   end
