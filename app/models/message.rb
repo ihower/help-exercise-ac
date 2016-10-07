06e550b4180f29b178b4f8ca18b27335e9a6f724
Zen <zuzen324@gmail.com>
2016-10-07 22:05:32 +0800

    filter message by scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,10 @@ class Message < ActiveRecord::Base
 
   has_many :comments, :dependent => :destroy
 
+  scope :pending, -> { where( :status => "pending" ) } 
+  scope :completed, -> { where( :status => "completed" ) } 
+  scope :within_days, ->(num) { where( "created_at >= ?", Time.now - num.days ) } 
+
   def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
   end
